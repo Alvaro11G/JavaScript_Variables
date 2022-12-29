62b2f739cfea2260d9bc1cab072097033d5b4ff1
Alvaro11G <agarcia11746@gmail.com>
2022-12-29 11:30:16 -0700

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName="Alvaro";
+let lastName="Garcia";
+var age = 20;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+var city = "Farmington";
+var pastime = "soccer";
+let myStory = `Hello my name is ${fullName}. I live in ${city} New Mexico. And my favorite hobby is ${pastime}`;
+console.log(myStory);
